6ba27f56d5f9a53aa7de04eb8eca0bd569ec25b3
0x07CB <83157348+0x07CB@users.noreply.github.com>
2021-06-17 19:52:44 +0000

    now I do start to do code for do thid do

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # exemple_crypto-mining
 exemple of cryptomining with the SHA-152
+
 in progress...
+
+**capacity's**
+	- [ ] multi-threads
+	- [ ] generator of exhaustive string combinaisons
+	- [ ] data-mining
+
+**usage exemple**
+	- [ ] hide better ssh keys
+	- [ ] generate great password to infinite passwords (WARNING: seeded random) 
+	- [ ] simple exemple step by step
--- dataMiner.py
+++ dataMiner.py
@@ -5,4 +5,27 @@ import base64
 from multiprocessing import Process, Pool
 import argparse
 
+possible_characts = range(0,255)
+
+class exhaustivitatatator(object):
+    def __init__(self,size=64):
+        self.size = size
+        self.init()
+    def next(self):
+        pass
+    def init(self):
+        self.value = chr(0) * self.size
+    def get(self):
+        return self.value
+    
+class dataMiner(object):
+    def __init__(self):
+        self.data = exhaustivitatatator()
+    def mining(self):
+        pass
+    def set_data(self):
+        pass
+    def hashfunc(self,salt):
+        hashlib.sha3_256(self.data.value,salt).hexdigest()
+
 
