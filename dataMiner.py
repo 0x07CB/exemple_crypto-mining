041b761070539a607f0f2fa432c388d9d0b36510
0x07CB <83157348+0x07CB@users.noreply.github.com>
2021-06-18 09:38:51 +0200

    Update dataMiner.py

--- dataMiner.py
+++ dataMiner.py
@@ -9,22 +9,36 @@ class exhaustivitatatator(object):
     def __init__(self,size=64):
         self.size = size
         self.init()
+        self.value=list(self.value)
     def next(self):
-        self.value[0] = chr((int(ord(self.value[0]))+1)%256)
-        for i in range
+        self.value[0] = chr(((int(ord(self.value[0]))+1)%256))
+        for i in range(0,self.size):
+            if self.value[0+i] == chr(0):
+                self.value[0+i+1] = chr((int(ord(self.value[0+i+1]))+1)%256)
+            else:
+                break
     def init(self):
         self.value = chr(0) * self.size
     def get(self):
         return self.value
     
 class dataMiner(object):
-    def __init__(self):
-        self.data = exhaustivitatatator()
-    def mining(self):
-        d
+    def __init__(self,size_block_validation=4):
+        self.salt = exhaustivitatatator()
+        self.size_block_validation = size_block_validation
+    def mining(self,tries=1000000,chrepet="0"):
+        for i in range(0,tries):
+            self.salt.next()
+            if self.hashfunc()[:self.size_block_validation] == chrepet * self.size_block_validation:
+                return i
     def set_data(self,data):
         self.data = data
-    def hashfunc(self,salt):
-        hashlib.sha3_256(self.data.value,salt).hexdigest()
+    def hashfunc(self):
+        return hashlib.sha3_256("".join(self.salt.get()+list(self.data)).encode()).hexdigest()
+
+dm = dataMiner()
+
+dm.set_data("test blabla")
+print (dm.mining())
 
 
