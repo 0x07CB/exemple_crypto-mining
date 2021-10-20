98ef8ea4fbdba63752c98fb1958332dca213aa80
0x07CB <83157348+0x07CB@users.noreply.github.com>
2021-10-20 21:56:12 +0000

    maj

--- dataMiner.py
+++ dataMiner.py
@@ -3,7 +3,7 @@
 
 # Need to keep in mind that is not automated so... 
 # U need to change manually this string for have an correct version number 
-__VERSION__ = "0.1.3"
+__VERSION__ = "0.1.5"
 
 # dataMiner.py
 # Author: Rick Sanchez [ D-634 ]
